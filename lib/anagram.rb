b98be958b17c92c2f651ecc28b723bf393c835c6
Jacob <jacobamelse@hotmail.com>
2023-01-01 18:39:08 -0600

    commit tree 54f0690fba4a01a35200c45dabf5fc12c18088b5

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+  def match(new_word)
+    matching = new_word.filter{|w| w.chars.sort == @word.chars.sort}
+    p matching
+  end
+end
+
+
+
